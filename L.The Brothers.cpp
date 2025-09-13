4633f473473241c0f14e32f95cbc2dac4d99883c
Sakiatun Zannat <sakia.zannat2023@gmail.com>
2025-09-13 20:49:17 +0600

    Add files via upload

---
+++ L.The Brothers.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+
+int main() {
+   string First1,Second1,First2,Second2;
+
+   cin>> First1>>Second1>>First2>>Second2;
+   
+   if (Second1 == Second2)
+   cout<<"ARE Brothers"<<endl;
+   else 
+   cout<<"NOT"<<endl;
+   return 0;
+}
