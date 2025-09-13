df5efdc7431842294777a26f5e29909698d54e25
Sakiatun Zannat <sakia.zannat2023@gmail.com>
2025-09-14 00:43:54 +0600

    Add files via upload

---
+++ P. First digit.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+
+int main() {
+    int a;
+    cin>>a;
+    if( (a/1000)%2 == 0 ){
+        cout<<"EVEN"<<endl;
+    }
+    else if((a/1000)%2 != 0){
+        cout<<"ODD"<<endl;
+    }
+    }
