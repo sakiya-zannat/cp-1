00c2db53e5aa9c7b4ddd68bc111aa91bf791f520
Sakiatun Zannat <sakia.zannat2023@gmail.com>
2025-09-13 23:25:51 +0600

    Add files via upload

---
+++ O. Calculator.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+
+int main() {
+    int a,b;
+    char c;
+
+    if(c=='+'){
+    
+    cout<<a+b;}
+
+    else if(c=='-'){
+    
+    cout<<a-b;}
+
+     else if(c=='*')
+   {    cout<<a*b;}
+
+       else if(c=='/'){
+       cout<< a/b;}
+    return 0;
+}
